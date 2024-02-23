d7edc9ca7d7de0e3ef77eb8ba1929630fc0d6f81
Divyarajsinh Jadeja <107667253+Divyarajjj7@users.noreply.github.com>
2024-02-23 10:54:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String args[])
     {
-        System.out.println("Java for Slack, Ngrok Pipeline and Email");
+        System.out.println("Java for Slack, Ngrok Pipeline and Email")
     } 
 }
